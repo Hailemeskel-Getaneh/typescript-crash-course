78f428059fe143ff11cf56f016bf8267d09279be
Hailemeskel-Getaneh <hailegetaneh1221@gmail.com>
2025-08-15 06:19:45 +0300

    tuples in typescript

---
+++ src/Foundamentals/6-tuples.ts
@@ -0,0 +1,6 @@
+// It is an array with specific length and specify the type of each element.
+
+ let data : [String, number , boolean] = ["Haile", 23, true]
+
+ data.push("Ethiopia"); // This will cause an error because the tuple is fixed length and types
+
