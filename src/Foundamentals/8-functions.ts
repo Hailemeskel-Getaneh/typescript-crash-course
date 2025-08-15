f558ffa1133af93d149f5245e0b47ea2b951a763
Hailemeskel-Getaneh <hailegetaneh1221@gmail.com>
2025-08-16 01:50:47 +0300

    functions in typescript

---
+++ src/Foundamentals/8-functions.ts
@@ -0,0 +1,11 @@
+// Functions are types of objects that can be called to perform actions or compute values.
+// Functions can take parameters and return values, and they can be defined in various ways, including
+// function declarations, function expressions, and arrow functions.    
+
+// Example: A simple function that adds two numbers
+function add(a: number, b: number): number {
+    return a + b;
+}       
+
+// It is better to annotate the function parameters and return type to ensure type safety and clarity.
+// In the above example, the number at the end of the function signature indicates that the function will return a number.
