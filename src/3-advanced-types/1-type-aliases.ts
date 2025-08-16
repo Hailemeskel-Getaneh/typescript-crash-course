38c8fb2992bb974d365dfc65a7fca4f08f45eced
Hailemeskel-Getaneh <hailegetaneh1221@gmail.com>
2025-08-16 16:27:34 +0300

    type aliases in typescript

---
+++ src/3-advanced-types/1-type-aliases.ts
@@ -0,0 +1,17 @@
+// Type aliases allow you to create a new custom name for a type.
+// This can be useful for simplifying complex types or creating more readable code.
+
+//  example
+
+type Employee ={
+    id: number;
+    name  :string;
+    age: number;
+
+}
+
+let new_employee: Employee = {
+    id : 123_455,
+    name : "Hailemeskel Getaneh",
+    age : 23
+}
