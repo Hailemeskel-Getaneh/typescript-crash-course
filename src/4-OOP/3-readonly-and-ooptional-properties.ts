b1fc03deadee40d6f143f8bb9fd27930fbc47124
Hailemeskel-Getaneh <hailegetaneh1221@gmail.com>
2025-08-20 05:05:08 +0300

    readonly and optional properties in OOP

---
+++ src/4-OOP/3-readonly-and-ooptional-properties.ts
@@ -0,0 +1,31 @@
+
+
+class Account {
+
+    readonly id : number; // id is readonly, it cannot be changed after initialization
+    name :string;
+    balance : number = 20;
+    nickname?: string; // nickname is optional, it can be undefined. not a mandatory to initialize it.
+
+    constructor(id: number, name: string, balance: number){
+        this.id = id,
+        this.name = name,
+        this.balance = balance
+    }
+
+    deposit( balance: number): void {
+        
+        if( balance < 0){
+            console.log('Invalid balance')
+        }
+        
+        this.balance = this.balance + balance;
+    }
+}
+
+let account1 = new Account(123, 'Hailemeskel Getaneh', 0)
+
+account1.deposit(120_000_000);
+console.log(account1.balance)
+
+console.log(account1 instanceof Account) // output should be true.
