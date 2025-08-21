b2eae4863e9652db2ff07b77e6548f4a7cf31628
Hailemeskel-Getaneh <hailegetaneh1221@gmail.com>
2025-08-21 10:12:59 +0300

    index signatures

---
+++ src/4-OOP/7-index-signatures.ts
@@ -0,0 +1,15 @@
+// Index signatures in TypeScript 
+
+ class StringArray {
+   [index: number]: string;
+   [studentId: string]: string; // Index signature for string keys
+
+ 
+ } 
+
+ let myArray = new StringArray();
+ myArray[0] = 'Hello';
+ myArray[1] = 'World';
+
+myArray.student1 = 'Hailemeskel'; // Assigning a string to a string index
+myArray.student2 = 'Mahi'
